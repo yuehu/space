04f8b101a1b9acc0c945f27e67ca10449655b390
Hsiaoming Yang <me@lepture.com>
2014-02-10 23:59:14 +0800

    Extends CJK members.

--- index.js
+++ index.js
@@ -4,12 +4,12 @@
 
 module.exports = function(text) {
   // CJK English
-  text = text.replace(/([\u4e00-\u9fa5\u3040-\u30FF])([a-z0-9@&=_\[\$\%\^\*\-\+\(\/\\])/ig, '$1 $2');
+  text = text.replace(/([\u4e00-\u9fff\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af])([a-z0-9@&=_\[\$\%\^\*\-\+\(\/\\])/ig, '$1 $2');
   // English CJK
-  text = text.replace(/([a-z0-9!~&;=_\]\,\.\:\?\$\%\^\*\-\+\)\/\\])([\u4e00-\u9fa5\u3040-\u30FF])/ig, '$1 $2');
+  text = text.replace(/([a-z0-9!~&;=_\]\,\.\:\?\$\%\^\*\-\+\)\/\\])([\u4e00-\u9fff\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af])/ig, '$1 $2');
   // CJK quote
-  text = text.replace(/([\u4e00-\u9fa5\u3040-\u30FF])(\"|\'(\S+))/ig, '$1 $2');
+  text = text.replace(/([\u4e00-\u9fff\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af])(\"|\'(\S+))/ig, '$1 $2');
   // quote CJK
-  text = text.replace(/((\S+)\'|\")([\u4e00-\u9fa5\u3040-\u30FF])/ig, '$1 $3');
+  text = text.replace(/((\S+)\'|\")([\u4e00-\u9fff\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af])/ig, '$1 $3');
   return text;
 };
